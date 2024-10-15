a2c7445418f972a3230e89f728bdbaec1739dd04
Chethan Kumar <chethankumars.v2016@gmail.com>
2024-10-15 22:26:26 +0530

    fix env

--- db/db.js
+++ db/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 export const Connection = async () => {
-    const URL = process.env.DB_URI
+    const URL = "mongodb+srv://chethan1234:neogchethan@neog.nxppkiu.mongodb.net/dataViz?retryWrites=true&w=majority&appName=neoG"
     try {
-        await mongoose.connect(URL, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        })
+        await mongoose.connect(`${URL}`)
         console.log("Database Connected Successfully.");
     } catch (error) {
         console.log("Error while connecting with database", error);
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`server is running on PORT ${PORT}`);
